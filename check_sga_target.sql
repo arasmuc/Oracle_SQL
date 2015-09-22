09591008378e5cf0dbde3fe7bf02dbc786f5bea0
arasmuc <arkadiuszborucki@gmail.com>
2015-09-22 23:02:57 +0200

    Create check_sga_target.sql

---
+++ check_sga_target.sql
@@ -0,0 +1,14 @@
+set heading off
+
+show parameter sga_target
+
+show parameter DB_CACHE_SIZE
+show parameter SHARED_POOL
+
+show parameter LARGE_POOL_SIZE
+show parameter JAVA_POOL_SIZE
+show parameter STREAMS_POOL_SIZE
+
+
+
+set heading on
