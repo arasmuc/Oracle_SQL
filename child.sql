b0fd340329fa2a1575705960eccc67aa1b88aa4a
arasmuc <arkadiuszborucki@gmail.com>
2016-03-23 20:09:26 +0100

    Create child.sql

---
+++ child.sql
@@ -0,0 +1,21 @@
+# child or parent tables on a table
+
+ 1) Query to find the child tables
+
+SELECT p.table_name PARENT_TABLE, c.table_name CHILD_TABLE
+FROM dba_constraints p, dba_constraints c
+WHERE (p.constraint_type = 'P' OR p.constraint_type = 'U')
+AND c.constraint_type = 'R'
+AND p.constraint_name = c.r_constraint_name
+AND p.table_name = UPPER('&PARENT_TABLE_NAME')
+and p.owner='&PARENT_TABLE_SCHEMA';
+
+2) Query to find the parent tables of a table
+
+SELECT c.table_name CHILD_TABLE, p.table_name PARENT_TABLE,c.owner CHILD_TABLE_OWNER,p.owner PARENT_TABLE_OWNER
+FROM dba_constraints p, dba_constraints c
+WHERE (p.constraint_type = 'P' OR p.constraint_type = 'U')
+AND c.constraint_type = 'R'
+AND p.constraint_name = c.r_constraint_name
+AND c.table_name = UPPER('&CHILD_TABLE_NAME')
+and c.owner='&CHILE_TABLE_SCHEMA'; --This condition is required in case you have multiple tables with same name
