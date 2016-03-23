edb856ae023f60224a33606ba6d77136a2125fba
arasmuc <arkadiuszborucki@gmail.com>
2016-03-23 10:29:11 +0100

    Create login.sql

---
+++ login.sql
@@ -0,0 +1,7 @@
+set linesize 10000
+show pagesize
+-- useful if displaying large XML docs that are brokn up with column headers
+--set pagesize 2000
+-- handle CLOBs (XML columns)
+set long 30000
+set longchunksize 30000
