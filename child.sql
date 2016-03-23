3f65337cce3a34931ae8fdbaf52374186b13752e
arasmuc <arkadiuszborucki@gmail.com>
2016-03-23 22:28:09 +0100

    Update child.sql

--- child.sql
+++ child.sql
@@ -19,3 +19,12 @@ AND c.constraint_type = 'R'
 AND p.constraint_name = c.r_constraint_name
 AND c.table_name = UPPER('&CHILD_TABLE_NAME')
 and c.owner='&CHILE_TABLE_SCHEMA'; --This condition is required in case you have multiple tables with same name
+
+eg:
+SELECT distinct  p.table_name PARENT_TABLE, c.table_name CHILD_TABLE
+FROM dba_constraints p, dba_constraints c
+WHERE (p.constraint_type = 'P' OR p.constraint_type = 'U')
+AND c.constraint_type = 'R'
+AND p.constraint_name = c.r_constraint_name
+AND p.table_name in (SELECT table_name  from all_tables where owner = 'ALMAGG')
+and p.owner='ALMAGG' order by p.table_name;
